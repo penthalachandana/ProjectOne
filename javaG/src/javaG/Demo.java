9f0f15478991b14f7f981c229bbe5df6c9248ab0
Chandana Penthala <penthalachandana@gmail.com>
2023-07-11 09:58:53 +0530

    Second Commit

--- javaG/src/javaG/Demo.java
+++ javaG/src/javaG/Demo.java
@@ -1,11 +1,16 @@
 package javaG;
 
 public class Demo {
+	void add()
+	{
+		System.out.println("add method");
+	}
 
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a);
-
+  Demo d=new Demo();
+  d.add();
 	}
 
 }
